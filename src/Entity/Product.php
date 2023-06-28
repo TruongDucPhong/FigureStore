230a64434b57cbc2854af948b8f2db063f032a54
Your Name <you@example.com>
2023-06-28 18:21:58 +0700

    4

---
+++ src/Entity/Product.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $productname = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProductname(): ?string
+    {
+        return $this->productname;
+    }
+
+    public function setProductname(string $productname): self
+    {
+        $this->productname = $productname;
+
+        return $this;
+    }
+}
